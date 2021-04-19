e67155e6c91765248666a51cce0e6c978d66b987
haru0418 <77143892+haru0418@users.noreply.github.com>
2021-04-19 23:07:49 +0900

    ルーティング編集

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,3 @@
 Rails.application.routes.draw do
-  get 'food_top/index'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'food_top#index'
 end
